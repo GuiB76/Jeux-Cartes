3d7ce6eb026e80e8d2edf10b4a4a482cda19b335
Guib76 <130552785+GuiB76@users.noreply.github.com>
2023-08-22 13:06:55 +0200

    Mise en place

---
+++ header/header.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() 
+{
+    const nosJeuxLink = document.getElementById('NosJeux-link');
+        const TchoytchoyLink = document.getElementById('Tchoytchoy-link');
+        const PMULink = document.getElementById('PMU-link');
+        const BiskitLink = document.getElementById('Biskit-link');
+        const MenteurLink = document.getElementById('Menteur-link');
+        const JeuDu7Link = document.getElementById('JeuDu7-link');
+        const LeCarréLink = document.getElementById('LeCarré-link');
+    const aProposLink = document.getElementById('about-link');
+    const contactLink = document.getElementById('contact-link');
+
+    // Changer les liens du menu de navigation
+    nosJeuxLink.href = 'nouveau_lien_our_work.html';
+        TchoytchoyLink.href = 'nouveau_lien_our_work.html';
+        PMULink.href = 'nouveau_lien_our_work.html';
+        BiskitLink.href = 'nouveau_lien_our_work.html';
+        MenteurLink.href = 'nouveau_lien_our_work.html';
+        JeuDu7Link.href = 'nouveau_lien_our_work.html';
+        LeCarréLink.href = 'nouveau_lien_our_work.html';
+    aProposLink.href = 'nouveau_lien_about.html';
+    contactLink.href = 'nouveau_lien_contact.html';
+});
+
+
