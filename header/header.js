3c305eb626aef982f53c4c8f09a5dbf24389bb9b
Guib76 <130552785+GuiB76@users.noreply.github.com>
2023-08-22 13:58:59 +0200

    zdqd

--- header/header.js
+++ header/header.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function()
     const nosJeuxLink = document.getElementById('NosJeux-link');
         const TchoytchoyLink = document.getElementById('Tchoytchoy-link');
         const PMULink = document.getElementById('PMU-link');
-        const BiskitLink = document.getElementById('Biskit-link');
+        const BizkitLink = document.getElementById('Bizkit-link');
         const MenteurLink = document.getElementById('Menteur-link');
         const JeuDu7Link = document.getElementById('JeuDu7-link');
         const LeCarréLink = document.getElementById('LeCarré-link');
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function()
     nosJeuxLink.href = 'nouveau_lien_our_work.html';
         TchoytchoyLink.href = 'nouveau_lien_our_work.html';
         PMULink.href = 'nouveau_lien_our_work.html';
-        BiskitLink.href = 'nouveau_lien_our_work.html';
+        BizkitLink.href = 'nouveau_lien_our_work.html';
         MenteurLink.href = 'nouveau_lien_our_work.html';
         JeuDu7Link.href = 'nouveau_lien_our_work.html';
         LeCarréLink.href = 'nouveau_lien_our_work.html';
