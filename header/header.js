f6abfda5b51c5dca1760cbe09101f0e81cb4a83b
Guib76 <130552785+GuiB76@users.noreply.github.com>
2023-08-23 15:40:29 +0200

    Bizkit v0.1

--- header/header.js
+++ header/header.js
@@ -11,15 +11,15 @@ document.addEventListener('DOMContentLoaded', function()
     const contactLink = document.getElementById('contact-link');
 
     // Changer les liens du menu de navigation
-    nosJeuxLink.href = 'nouveau_lien_our_work.html';
-        TchoytchoyLink.href = 'nouveau_lien_our_work.html';
-        PMULink.href = 'nouveau_lien_our_work.html';
+    nosJeuxLink.href = '#';
+        TchoytchoyLink.href = '#';
+        PMULink.href = '#';
         BizkitLink.href = 'bizkit-explications.html';
-        MenteurLink.href = 'nouveau_lien_our_work.html';
-        JeuDu7Link.href = 'nouveau_lien_our_work.html';
-        LeCarréLink.href = 'nouveau_lien_our_work.html';
-    aProposLink.href = 'nouveau_lien_about.html';
-    contactLink.href = 'nouveau_lien_contact.html';
+        MenteurLink.href = '#';
+        JeuDu7Link.href = '#';
+        LeCarréLink.href = '#';
+    aProposLink.href = '#';
+    contactLink.href = '#';
 });
 
 
